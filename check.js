8bac5c261e8870ab9b763ae21ba0b3b9d885be33
Nwe Nwe Lin <59752266+nwenwelin1@users.noreply.github.com>
2020-07-07 11:23:08 +0630

    Add files via upload

--- check.js
+++ check.js
@@ -1,36 +1,13 @@
-var n1,n2,i,result="";
-function isCheck(){
-    n1=parseInt(document.getElementById("num1").value);
-    n2=parseInt(document.getElementById("num2").value);
-    result+="<table>";
-    if(n1<n2){
-        for(i=n1; i<n2+1;i++){
-            result+="<tr>";
-            if(i%2==0){
-                 result+="<td>"+i+"</td><td>=even</td>";
-            }
-            else{
-                result+="<td>"+i+"</td><td>=Odd</td>";
-            }
-            result+="</tr>";
-        }
+function check(){
+    var inputDate=document.getElementById("date").value;
+    var birthday=new Date(inputDate);
+    var now=new Date();
+    var result=Math.abs(now.getFullYear()-birthday.getFullYear());
+    if(parseInt(result)<18){
+        document.getElementById("result").innerHTML="less than 18";
     }
-    else if(n1>n2){
-        var first=n2;
-        var second=n1;
-        for(i=first; i<second+1;i++){
-            result+="<tr>";
-            if(i%2==0){
-                result+="<td>"+i+"</td><td>=even</td>";
-            }
-            else{
-                result+="<td>"+i+"</td><td>=Odd</td>";
-            }
-            result+="</tr>"; 
-        }
-        
+    else{
+        document.getElementById("result").innerHTML="greater than 18";
     }
-    result+="</table>";
-    document.getElementById("display").innerHTML=result;
-
+    
 }
