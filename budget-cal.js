4b901fe1d1f6cf1f06d05ade08245d81c251cb0f
Nwe Nwe Lin <59752266+nwenwelin1@users.noreply.github.com>
2020-07-09 12:19:51 +0630

    Add files via upload

--- budget-cal.js
+++ budget-cal.js
@@ -4,37 +4,52 @@ function getBudget(){
     totalBudget+=parseInt(budget);
     $("#bLabel").text(totalBudget);
     getBalance();
+    clearText();
 }
 
-var n=0;
+var n=0,str="";;
 var totalExpense=0;
-
 var expenseList=[],iconList=[];
-
 function getAddExpense(){
     var exName=$("#inputExpense").val();
     var exAmt=$("#inputEAmount").val() || 0;
-
     var ex={title:exName,amt:exAmt};
     expenseList.push(ex);
-
-    var icon={edit:"<i class='fa fa-edit' style='font-size:20px;'></i>",delete:"<i class='fa fa-trash' style='font-size:20px'></i>"};
+    var icon={edit:"<i class='fa fa-edit' style='font-size:15px;'></i>",delete:"<i class='fa fa-trash' style='font-size:15px'></i>"};
     iconList.push(icon);
-    
-    var str="";
     totalExpense=0;
-   
     expenseList.forEach(function(value,key){
         totalExpense+=parseInt(value.amt);
-        str="<tr><td>"+value.title+"</td><td>"+value.amt+"</td><td>"+iconList[key].edit+"&nbsp;"+iconList[key].delete+"</td></tr>";
+        str="<tr id='"+key+"'><td>"+value.title+"</td><td>"+value.amt+"</td><td><button onclick='editItem("+key+");'>"+iconList[key].edit+"</button><button onclick='deleteItem("+key+");'>"+iconList[key].delete+"</button></td></tr>";
         
     });
    $("#disp").append(str);
    $("#expense").text(totalExpense);
     getBalance();
+    clearText();
 }
 
 function getBalance(){
-    //to get balance
     $("#balance").text(parseInt(totalBudget)-parseInt(totalExpense));
+}
+// function deleteItem(k){
+//     $("#"+k).remove();
+//     totalExpense-=expenseList[k].amt;
+//     $("#expense").text(totalExpense);
+//     delete expenseList[k];
+//     delete iconList[k]; 
+//     getBalance();
+// }
+
+function editItem(i){
+    $("#inputExpense").text(expenseList[i].title);
+    $("#inputEAmount").text(expenseList[i].amt);
+        var fieldID = $("#"+i).prev().attr("id");
+        $('#' + fieldID).val("hello world");
+}
+
+function clearText(){
+    $("#inputExpense").val(" ");
+    $("#inputEAmount").val(" ");
+    $("#inputBudget").val(" ");
 }
